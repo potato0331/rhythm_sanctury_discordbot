6207fe240fb297a04c1daa4069592fd0f9c3316e
potato0331 <mathcdy@naver.com>
2025-11-07 14:37:02 +0000

    공산당 카드 제작중

--- cogs/Card_draw.py
+++ cogs/Card_draw.py
@@ -1,8 +1,8 @@
 import asyncio
 import discord
 from discord.ext import commands
-from models import Card
-from discord import app_commands
+from models import Card, Player
+from discord import app_commands, ui
 import config
 import card_list
 import random 
@@ -13,40 +13,15 @@ class CardDraw(commands.Cog):
         self.bot.card_deck: list[Card] = []
         self.__deck_reset()
 
+
     def __deck_reset(self):
         for card in card_list.CARDS: #덱 리셋
             for i in range(card.card_count):
                 self.bot.card_deck.append(card)
         random.shuffle(self.bot.card_deck)
-        
 
-    @app_commands.command(name="카드뽑기", description=f"{config.CARD_PRICE}코인을 지불하고 무작위의 찬스카드 1장을 뽑습니다.")
-    async def _draw_card(self, interaction: discord.Interaction):
-        player = None
-        for status in self.bot.player_status:
-            if status.name == interaction.user.global_name:
-                player = status
-                break
 
-        #예외처리
-        if(player == None):
-            await interaction.response.send_message(f"{interaction.user.global_name}님은 플레이어가 아닙니다. 또는 알 수 없는 오류가 발생했습니다.",ephemeral = True)
-            return
-        if(self.bot.current_phase != config.Phase.CARD):
-            await interaction.response.send_message("지금은 카드를 뽑을 수 없습니다.",ephemeral=True)
-            return
-        if(player.coin < self.bot.current_card_price):
-            await interaction.response.send_message("코인이 부족합니다.",ephemeral = True)
-            return
-
-        #코인소모
-        player.coin -= self.bot.current_card_price
-
-        #카드 뽑기
-        if(len(self.bot.card_deck) == 0):
-            self.__deck_reset()
-        card = self.bot.card_deck.pop()
-
+    async def card_reveal(self, interaction: discord.Interaction, card: Card):
         #카드 공표용 엠베드 제작
         embed = discord.Embed()
 
@@ -65,13 +40,40 @@ class CardDraw(commands.Cog):
             embed.title = f"{card.name}"
             embed.add_field(name="효과", value=card.description, inline=True)
             embed.add_field(name="가산값", value=str(card.betting_value), inline=True)
-
-        await interaction.response.send_message(embed=embed)
-
-    
-        #카드 효과 적용
-        player.round_multiplier += card.betting_value
         
+        return embed
+
+    async def communist(self, interaction: discord.Interaction, player: Player):
+
+        async def draw_button_callback():      
+            await self.communist(interaction, player)
+            await interaction.message.delete()
+
+        async def accept_button_callback():      
+            player.round_multiplier += card.betting_value
+            await self.card_effect(interaction, card, player)
+            embed = await self.card_reveal(interaction, card)
+            await interaction.followup.send(content = "확정된 카드는...", embed = embed)
+            await interaction.message.delete()
+
+        
+        card = card_list.CARDS[random.randint(1,50) - 1]
+
+        draw_button = discord.ui.Button(style=discord.ButtonStyle.secondary, label="다시뽑기")
+        accept_button = discord.ui.Button(style=discord.ButtonStyle.success, label="확정하기")
+        draw_button.callback = draw_button_callback()
+        accept_button. callback = accept_button_callback()
+
+        view = discord.ui.View()
+        view.add_item(draw_button)
+        view.add_item(accept_button)
+
+        embed = await self.card_reveal(interaction, card)
+        await interaction.followup.send(content = "카드를 뽑습니다!", embed = embed, view=view)
+
+
+    async def card_effect(self, interaction: discord.Interaction, card: Card, player: Player):
+        #카드 태그별로 효과적용하는 함수 (배팅 가산값은 적용하지 않음.)
         match card.effect_tag:
             case "효과해제":
                 player.effect_list.clear()
@@ -118,9 +120,51 @@ class CardDraw(commands.Cog):
                 player.saved_pension += player.round_multiplier
                 player.round_multiplier = 0
 
-                             
-            case _:
+            case "공산당":
+                await self.communist(interaction, card, player)
+      
+            case _: #그 외 전부
                 player.effect_list.append(card.effect_tag)
+
+        
+
+    @app_commands.command(name="카드뽑기", description = f"{config.CARD_PRICE}코인을 지불하고 무작위의 찬스카드 1장을 뽑습니다.")
+    async def _draw_card(self, interaction: discord.Interaction):
+        player = None
+        for status in self.bot.player_status:
+            if status.name == interaction.user.global_name:
+                player = status
+                break
+
+        #예외처리
+        if(player == None):
+            await interaction.response.send_message(f"{interaction.user.global_name}님은 플레이어가 아닙니다. 또는 알 수 없는 오류가 발생했습니다.",ephemeral = True)
+            return
+        if(self.bot.current_phase != config.Phase.CARD):
+            await interaction.response.send_message("지금은 카드를 뽑을 수 없습니다.",ephemeral=True)
+            return
+        if(player.coin < self.bot.current_card_price):
+            await interaction.response.send_message("코인이 부족합니다.",ephemeral = True)
+            return
+
+        #코인 소모
+        player.coin -= self.bot.current_card_price
+
+        #카드 뽑기
+        if(len(self.bot.card_deck) == 0):
+            self.__deck_reset()
+        card = self.bot.card_deck.pop()
+
+        #카드 공개
+        embed = await self.card_reveal(interaction, card)
+        await interaction.response.send_message(embed=embed)
+
+        #카드 효과 적용
+        player.round_multiplier += card.betting_value
+        await self.card_effect(interaction, card, player)
+
+        
+       
 
 
 async def setup(bot: commands.Bot):
